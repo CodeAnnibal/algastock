4743743446e8a08bb8b72f60f5755a818c97b349
Annibal Silva <hannibaldaturn@gmail.com>
2020-12-13 01:12:35 -0300

    add logs

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 
 class ClassComponent extends React.Component<{ name: string}> {
 
+    constructor(props: any){
+        super(props);
+        console.log('constructor reached');
+    }
     state = {
         name: 'Mundo!!!'
     }
 
+    componentDidMount () {
+        console.log('did mount reached');
+    }
+
+    componentDidUpdate () {
+        console.log('did update reached');
+    }
+
     render () {
+        console.log('render reached');
     //return <div>Olá, {this.state.name}. Eu sou um componente baseado em classe!</div>
     //return <div>Olá, {this.props.name}. Eu sou um componente baseado em classe!</div>
     return <div>
