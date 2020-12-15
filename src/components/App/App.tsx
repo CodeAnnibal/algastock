ea12657c93a3712fb953c7a0df0a2bb1440fe198
Annibal Silva <hannibaldaturn@gmail.com>
2020-12-15 00:01:55 -0300

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestComponent from '../TestComponent'
+
 // import ClassComponent from '../ClassComponent/ClassComponent'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent name='Annibal' />
-        {/* <ClassComponent name='Mundo' /> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     App
     </div>
   );
 }
