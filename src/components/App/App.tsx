b971edb4aa2512f483b784c3489afb9eda5bd566
Annibal Silva <hannibaldaturn@gmail.com>
2020-12-20 01:31:16 -0300

    add header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../Header';
 import './App.css';
 
 // import ClassComponent from '../ClassComponent/ClassComponent'
 function App() {
   return (
     <div className="App">
+      <Header title="AlgaStock"/>
      App
     </div>
   );
