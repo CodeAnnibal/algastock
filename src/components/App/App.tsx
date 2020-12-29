04dc913721253984042f55309bd5216a91c64f7c
Annibal Silva <hannibaldaturn@gmail.com>
2020-12-29 00:42:19 -0300

    clear app + add table component's base files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,16 @@
-import React, { useState } from 'react';
-import Button from '../../shared/Button/Button';
+import React from 'react';
 import Container from '../../shared/Container/Container';
-import Input from '../../shared/Input';
 import Header from '../Header';
 import './App.css';
 
-function TestComponent () {
-  return <img 
-            width="16"
-            src="https://img.icons8.com/pastel-glyph/452/search--v2.png"
-            alt="search icon" />
-}
-
 // import ClassComponent from '../ClassComponent/ClassComponent'
 function App() {
-  const [street, setStreet] = useState('')
-
+ 
   return (
     <div className="App">
       <Header title="AlgaStock"/>
       <Container>
-        <ul>
-          {
-            ['Daniel','Willian','Thiago','Daniel'].map((name, index) => {
-              return <li key={index}>
-                { name }
-              </li>
-            })
-          }
-        </ul>
+       
       </Container>
     </div>
   );
---
+++ src/shared/Table/Table.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Table.scss'
+
+const Table = () => {
+    return <table>
+        Table
+    </table>
+}
+
+export default Table
