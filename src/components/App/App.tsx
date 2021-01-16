80e59e957612d3bd6cff2fe6fd7ff29ff13084b5
Annibal Silva <hannibaldaturn@gmail.com>
2021-01-16 01:22:24 -0300

    add form component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Button from '../../shared/Button/Button';
 import Container from '../../shared/Container/Container';
+import Form from '../../shared/Form';
+import Input from '../../shared/Input';
 import Table, { TableHeader } from '../../shared/Table';
 import Products from '../../shared/Table/Table.mockdata';
 import Header from '../Header';
@@ -22,6 +25,26 @@ function App() {
         <Table 
           headers={ headers } data={Products}
         />
+
+        <Form title="Product Form" onSubmit={console.log}>
+          <Input 
+            label="Name"
+            placeholder="E.g.: Cookie"/>
+          <Input 
+            label="Price"
+            type="number"
+            step="0.01"
+            min="0"
+            placeholder="E.g.: 1.25"/>
+          <Input 
+            label="Stock"
+            type="number"
+            min="0"
+            placeholder="E.g.: 15"/>
+            <Button>
+              Submit
+            </Button>
+        </Form>
       </Container>
     </div>
   );
